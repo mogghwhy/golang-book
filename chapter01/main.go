f066d0652247333b58e443559ac1ad10037168e0
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2022-08-30 21:33:59 +0300

    added comments to main.go

--- chapter01/main.go
+++ chapter01/main.go
@@ -1,5 +1,13 @@
 package main
 
+/*
+creates an executable program that references the fmt library and contains
+one function. that function takes no arguments and doesn't return anything.
+It accesses the println function contained inside the fmt package and
+invokes it using one argument - the string Hello, World
+*/
+
+// import fmt package
 import "fmt"
 
 func main() {
