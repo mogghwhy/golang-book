a885abe17560e6ca51d6b01e7e7c9d23602b44e1
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2022-09-15 20:02:37 +0000

    Thu, Sep 15, 2022, 8:02 PM +00:00

--- chapter03/exercise05.go
+++ chapter03/exercise05.go
@@ -6,6 +6,6 @@ func main() {
 	fmt.Print("Enter degrees in fahrenhei:")
 	var input float64
 	fmt.Scanf("%f", &input)
-	output := (input - 32) * (5 / 9)
-	fmt.Println(output)
+	output := (input - 32.0) * (5.0 / 9.0)
+	fmt.Printf("%v degrees fahrenhei is %2.2f degrees celsius\n",input,output)
 }
