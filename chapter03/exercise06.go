b693def5fc7296edb4d7bd497cfb2cd4fed6077e
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2022-09-15 20:05:43 +0000

    Thu, Sep 15, 2022, 8:05 PM +00:00

--- chapter03/exercise06.go
+++ chapter03/exercise06.go
@@ -6,6 +6,6 @@ func main() {
 	fmt.Print("Enter length in feet:")
 	var input float64
 	fmt.Scanf("%f", &input)
-	output := (input 0.3048)
+	output := (input * 0.3048)
 	fmt.Printf("%v feet is %2.2f meters\n",input,output)
 }
