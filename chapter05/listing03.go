0806a5c19be36ee81a1f1cf65c7a72bf04d2d17d
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2022-09-22 11:52:36 +0000

    different array declaration

--- chapter05/listing03.go
+++ chapter05/listing03.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var x [5]float64{98,93,77,82,83,}
+	x := [5]float64{98, 93, 77, 82, 83}
 	var total float64 = 0
 	for _, value := range x {
 		total += value
