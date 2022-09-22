3588ad5744023e0134b74f3bb0121de813be238b
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2022-09-22 09:43:04 +0000

    Thu, Sep 22, 2022, 9:43 AM +00:00

--- chapter04/exercise03.go
+++ chapter04/exercise03.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	for i := 1; i <= 100; i++ {
-		if i%3 == 0 && i%5 {
+		if i%3 == 0 && i%5 == 0 {
 			fmt.Println("FizzBuzz")
 		} else if i%3 == 0 {
 			fmt.Println("Fizz")
